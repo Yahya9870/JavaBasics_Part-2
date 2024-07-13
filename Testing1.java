434332353de373e2187e27f785a9181c22dff42b
Yahya9870 <56015489+Yahya9870@users.noreply.github.com>
2024-07-13 19:57:35 -0400

    just first commit

---
+++ Testing1.java
@@ -0,0 +1,9 @@
+public class Testing1 {
+    public static void main(String[] args) {
+        System.out.println("Sushil"); // Sushil will get printed and bcoz of ln cursor will go to the next line.
+        System.out.println("is from Nepal"); // here also after printing the cursor goes to the next line.
+        System.out.print("Currently studying SE in States");
+        System.out.print("Something");
+    }
+}
+//yahiya.jmh@gmail.com
